6fedec635e0652659f998b6403e25a008fb2bc96
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-04-04 18:13:20 +0900

    feat: counter.vue

---
+++ src/js/components/counter.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    value : {{value}}
+    <button @click="increment()">+</button>
+    <button @click="decrement()">-</button>
+</div>
+</template>
+
+<script>
+import store from '../store/index'
+
+export default {
+    data(){
+        return {
+            value: store.getState(),
+        }
+    },
+    mounted(){
+        store.subscribe(() => {
+            this.value = store.getState();
+        });
+    },
+    methods: {
+        increment(){
+            store.dispatch({type: "INCREMENT"});
+        },
+        decrement(){
+            store.dispatch({type: "DECREMENT"});
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/js/index.vue
+++ src/js/index.vue
@@ -1,32 +1,15 @@
 <template>
 <div>
-    value : {{value}}
-    <button @click="increment()">+</button>
-    <button @click="decrement()">-</button>
+    <counter></counter>
 </div>
 </template>
 
 <script>
-import store from './store/index'
+import counterComponent from './components/counter.vue'
 
 export default {
-    data(){
-        return {
-            value: store.getState(),
-        }
-    },
-    mounted(){
-        store.subscribe(() => {
-            this.value = store.getState();
-        });
-    },
-    methods: {
-        increment(){
-            store.dispatch({type: "INCREMENT"});
-        },
-        decrement(){
-            store.dispatch({type: "DECREMENT"});
-        }
+    components: {
+        "counter" : counterComponent,
     }
 }
 </script>
